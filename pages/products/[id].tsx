a0f8f2cb238dc48089c1d72d5e4e1d658b341ab8
Le Trong Dat <datlt2306@gmail.com>
2022-06-28 12:27:11 +0700

    update

---
+++ pages/products/[id].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+type Props = {}
+
+const ProductDetail = (props: Props) => {
+    const router = useRouter();
+    const id = router.query.id;
+  return (
+    <div>ProductDetail id: {id}</div>
+  )
+}
+
+export default ProductDetail
