bffaf3da2602af40943ef3aaf84a201058aebee9
Le Trong Dat <datlt2306@gmail.com>
2022-06-30 13:18:52 +0700

    getStaticPaths

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,14 +1,34 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-type Props = {}
+type ProductProps = {
+  product: any;
+}
 
-const ProductDetail = (props: Props) => {
-    const router = useRouter();
-    console.log('router', router.query);
+const ProductDetail = ({product}: ProductProps) => {
+  if(!product) return null;
   return (
-    <div>ProductDetail</div>
+    <div>{product.name}</div>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`)).json();
+  const paths = data.map(product => (
+    { params: { id: product.id } }
+  ))
+  return {
+    paths,
+    fallback: false
+  }
+}
+// server
+export const getStaticProps: GetStaticProps<ProductProps> = async (context: GetStaticPropsContext) => {
+  const product = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params?.id}`)).json();
+  return {
+    props: {product}
+  }
 }
 
 export default ProductDetail
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,4 +1,5 @@
-import { GetStaticProps } from 'next'
+import { GetStaticProps, GetStaticPropsContext } from 'next'
+import Link from 'next/link'
 import React from 'react'
 
 type ProductsProps = {
@@ -10,21 +11,21 @@ const Products = ({products}: ProductsProps) => {
   if(!products) return null;
   return (
     <div>{products.map(item => (
-      <div key={item.id}>{item.name}</div>
+      <div key={item.id}><Link href={`/products/${item.id}`}>{item.name}</Link></div>
     ))}</div>
   )
 }
 
 
 // server
-export const getStaticProps: GetStaticProps<ProductsProps> = async (context) => {
+export const getStaticProps: GetStaticProps<ProductsProps> = async (context: GetStaticPropsContext) => {
   console.log('getStaticProps');
   const response = await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`);
   const data = await response.json();
 
   return {
     props: {
-      products: data
+      products: data.map(item => ({id: item.id, name: item.name}))
     }
   }
 }
