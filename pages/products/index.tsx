53808a0aeb3297210b8aa16c6e08b87e29704f61
Le Trong Dat <datlt2306@gmail.com>
2022-06-30 12:34:19 +0700

    getStaticProps with data

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,34 @@
+import { GetStaticProps } from 'next'
 import React from 'react'
 
-type Props = {}
-
-const Products = (props: Props) => {
+type ProductsProps = {
+  products: any[]
+}
+// client
+const Products = ({products}: ProductsProps) => {
+  console.log('Product component', products)
+  if(!products) return null;
   return (
-    <div>Products</div>
+    <div>{products.map(item => (
+      <div key={item.id}>{item.name}</div>
+    ))}</div>
   )
 }
 
+
+// server
+export const getStaticProps: GetStaticProps<ProductsProps> = async (context) => {
+  console.log('getStaticProps');
+  const response = await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`);
+  const data = await response.json();
+
+  return {
+    props: {
+      products: data
+    }
+  }
+}
 export default Products
+
+// getStaticProps without data
+// getStaticProps with data
