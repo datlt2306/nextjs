97c1fd1f3202b54a5fc31e273a990786de2e0b3d
Le Trong Dat <datlt2306@gmail.com>
2022-07-02 11:11:33 +0700

    update

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -14,55 +14,48 @@ const ProductDetail = ({product}: ProductProps) => {
 }
 
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const data = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`)).json();
-  console.log('data', data);
-  const paths = data.map(item => {
-    return { params: { id: item.id} }
-  })
-  return {
-    paths,
-    fallback: 'blocking'
-  }
-}
-
-export const getStaticProps: GetStaticProps<ProductProps> = async (
-  context: GetStaticPropsContext
-) => {
-  const data = await ( await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params.id}`)).json()
-  if(!data){
-    return {
-      notFound: true
-    }
-  }
-
-  return {
-      props: {
-          product: data,
-      },
-      revalidate: 10
-  };
-};
-
-// export const getServerSideProps: GetServerSideProps = async ( context: GetServerSidePropsContext) => {
-//   context.res.setHeader(
-//     'Cache-Control',
-//     'public, s-maxage=10, stale-while-revalidate=59'
-//   )
-//   const data = await ( await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params.id}`)).json()
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const data = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`)).json();
+//   console.log('data', data);
+//   const paths = data.map(item => {
+//     return { params: { id: item.id} }
+//   })
 //   return {
-//     props: {
-//       product: data
-//     }
+//     paths,
+//     fallback: 'blocking'
 //   }
 // }
 
+// export const getStaticProps: GetStaticProps<ProductProps> = async (
+//   context: GetStaticPropsContext
+// ) => {
+//   const data = await ( await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params.id}`)).json()
+//   if(!data){
+//     return {
+//       notFound: true
+//     }
+//   }
+
+//   return {
+//       props: {
+//           product: data,
+//       },
+//       revalidate: 10
+//   };
+// };
+
+export const getServerSideProps: GetServerSideProps = async ( context: GetServerSidePropsContext) => {
+  context.res.setHeader(
+    'Cache-Control',
+    's-maxage=5, stale-while-revalidate'
+  )
+  const data = await ( await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params.id}`)).json()
+  return {
+    props: {
+      product: data
+    }
+  }
+}
+
 export default ProductDetail
 
-
-
-// Static Site Generation => Render ra cac file html co san
-// Server Side rendering => server trả về dữ liệu khi user truy cap
-// Client Side rendering => giống react
-// Icremental Static Generation => 
-// cache dữ liệu, khi truy cập vào 1 page không có thì sẽ chờ thời gian tạo page mới
